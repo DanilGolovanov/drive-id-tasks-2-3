d0118867df16f8e49c0448e125d8f8f3d790d037
DanilGolovanov <golovanov.da1@gmail.com>
2021-11-13 20:23:14 +1100

    data is succesfully fetched from the API endpoint

--- task2.php
+++ task2.php
@@ -1,4 +1,16 @@
 <?php 
-    $str = "Hello PHP\n";
+    $API_URL = 'https://api.publicapis.org/entries';
+
+    // fetch data and read JSON file
+    $json_data = file_get_contents($API_URL);
+
+    // decode JSON data to array
+    $response_data = json_decode($json_data);
+
+    // retrieve info from response data
+    $entries_count = $response_data->count;
+    $entries_data = $response_data->entries;
+
+    $str = $entries_data[0]->API;
     echo $str;
 ?>
