e10d8c376291ff36cd1cd166ef74b238bd05708b
DanilGolovanov <golovanov.da1@gmail.com>
2021-11-13 21:33:16 +1100

    finished task2

--- task2.php
+++ task2.php
@@ -1,7 +1,15 @@
 <?php 
-    $API_URL = 'https://api.publicapis.org/entries';
-
+    // get provided arguments
+    if ($argc == 3) {
+        $category = $argv[1];
+        $limit = $argv[2];
+    } else { // handle cases with wrong inputs
+        echo "Wrong input.\nUsage: php file.php category limit";
+        return;
+    }
+    
     // fetch data and read JSON file
+    $API_URL = "https://api.publicapis.org/entries";
     $json_data = file_get_contents($API_URL);
 
     // decode JSON data to array
@@ -11,6 +19,24 @@
     $entries_count = $response_data->count;
     $entries_data = $response_data->entries;
 
-    $str = $entries_data[0]->API;
-    echo $str;
+    // sort and print APIs in descending alphabetical order
+    usort($entries_data, fn($a, $b) => -1 * strcmp($a->API, $b->API));
+
+    $counter = 0;
+
+    // loop through array of entries and print ones with matching category
+    foreach ($entries_data as $entry) {
+        if ($counter == $limit) { // make sure we not going over the limit
+            break;
+        }
+        if ($entry->Category == $category) {
+            echo $entry->API . "\n";
+            $counter++;
+        }
+    }
+
+    // print message if no entries matched provided category
+    if ($counter == 0) {
+        echo "No results";
+    }
 ?>
